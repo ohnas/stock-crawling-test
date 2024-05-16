5418af77f3e3b7c93d117dedc0bab2bff91eb484
ohnas <ohnas12@gmail.com>
2024-05-16 17:11:22 +0900

    sheets.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://kr.investing.com/equities/google-inc-historical-data"
+URL = "https://kr.investing.com/etfs/vanguard-s-p-500"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
 }
@@ -11,4 +11,6 @@ status = response.status_code
 print(status)
 html = response.text
 soup = BeautifulSoup(html, "html.parser")
-print(soup)
+div = soup.find("div", {"data-test": "instrument-price-last"})
+
+print(div.text)
---
+++ sheets.py
@@ -0,0 +1,60 @@
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = "13nCB53t-9u4OnGV_js84XEXmyvN81rnzX0nSPtxkNDs"
+RANGE_NAME = "stock_history_DB!A1:F"
+
+
+def sheet_oauth():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    return creds
+
+
+def append_values():
+    creds = sheet_oauth()
+
+    try:
+        service = build("sheets", "v4", credentials=creds)
+        service.spreadsheets().values().append(
+            spreadsheetId=SPREADSHEET_ID,
+            range=RANGE_NAME,
+            valueInputOption="USER_ENTERED",
+            body={"values": [[1, 1, 1, 1, 1, 1]]},
+        ).execute()
+        print("success")
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+
+if __name__ == "__main__":
+    append_values()

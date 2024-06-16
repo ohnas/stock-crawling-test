db69d075b2956b3a914c399c709a8acda94f8cc7
ohnas <ohnas12@gmail.com>
2024-06-16 15:50:00 +0900

    auth change

--- sheets.py
+++ sheets.py
@@ -1,59 +1,24 @@
-import os.path
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+import os
+from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
-from google.auth.exceptions import RefreshError
 
-
-# If modifying these scopes, delete the file token.json.
+SERVICE_ACCOUNT_FILE = os.path.join(os.getcwd(), "stock-crawling-test.json")
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-
-# The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = "13nCB53t-9u4OnGV_js84XEXmyvN81rnzX0nSPtxkNDs"
 
 
-def sheet_oauth():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    try:
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    "credentials.json", SCOPES
-                )
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open("token.json", "w") as token:
-                token.write(creds.to_json())
-    except RefreshError:
-        # If a RefreshError occurs, delete the token file and get a new token
-        if os.path.exists("token.json"):
-            os.remove("token.json")
-        flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-        creds = flow.run_local_server(port=0)
-        # Save the new credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
-
-    return creds
+def get_service():
+    credentials = Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES
+    )
+    service = build("sheets", "v4", credentials=credentials)
+    return service
 
 
 def append_values(values):
-    creds = sheet_oauth()
+    service = get_service()
     range_name = "stock_history_DB!A1:F"
 
-    service = build("sheets", "v4", credentials=creds)
     service.spreadsheets().values().append(
         spreadsheetId=SPREADSHEET_ID,
         range=range_name,
@@ -63,10 +28,9 @@ def append_values(values):
 
 
 def append_logs(date, result, error, type, stock):
-    creds = sheet_oauth()
+    service = get_service()
     range_name = "logs!A1:E"
 
-    service = build("sheets", "v4", credentials=creds)
     service.spreadsheets().values().append(
         spreadsheetId=SPREADSHEET_ID,
         range=range_name,
